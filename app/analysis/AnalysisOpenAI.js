5b1bf4861de6c524d27b868767e23bd80798c3df
Ferhat <fsarikaya96@hotmail.com>
2024-12-26 10:17:05 +0300

    initial commit

--- app/analysis/AnalysisOpenAI.js
+++ app/analysis/AnalysisOpenAI.js
@@ -18,15 +18,17 @@ export default function AnalysisOpenAI({ data }) {
     if (!response) {
       return <div>Yükleniyor...</div>;
     }
-    console.log(response.errors)
   
   return (
     <div>
       <h3 className="text-3xl font-bold text-center mb-6 mt-10 text-gray-800">OpenAI Hata Analizi</h3>
 
       <div className="mx-auto bg-white p-6 rounded-lg shadow-lg">
+      <p className="mb-6 text-lg">
+          <strong className="text-xl text-blue-600">Orjinal Metin:</strong> <span className="text-gray-700"> {response.original_text} </span>
+        </p>
         <p className="mb-6 text-lg">
-          <strong className="text-xl text-blue-600">Transkripte:</strong> <span
+          <strong className="text-xl text-blue-600">Transkripte Metin:</strong> <span
             className="text-gray-700"
             dangerouslySetInnerHTML={{
               __html: highlightAndRemoveBraces(response.text),
@@ -50,6 +52,7 @@ export default function AnalysisOpenAI({ data }) {
               <th className="px-4 py-2 border-b text-left">Eksik (Omission Errors)</th>
               <th className="px-4 py-2 border-b text-left">Tekrar (Repetition Errors)</th>
               <th className="px-4 py-2 border-b text-left">Tersine Çevirme (Reversal Errors)</th>
+              <th className="px-4 py-2 border-b text-left">Diğer Hatalar (Other Errors)</th>
             </tr>
           </thead>
           <tbody>
@@ -58,6 +61,7 @@ export default function AnalysisOpenAI({ data }) {
               <td className="px-4 py-2 border-b">{response.errors?.omission_errors}</td>
               <td className="px-4 py-2 border-b">{response.errors?.repetition_errors}</td>
               <td className="px-4 py-2 border-b">{response.errors?.reversal_errors}</td>
+              <td className="px-4 py-2 border-b">{response.errors?.other_errors}</td>
             </tr>
         </tbody>
       </table>
